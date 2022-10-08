484466661902ab914a430b9aedfb5ae267968855
divyanshmishra <79618723+Divyanshmishra101010@users.noreply.github.com>
2022-10-08 17:15:36 +0530

    Update Vector_STL.cpp

--- Vector_STL.cpp
+++ Vector_STL.cpp
@@ -5,19 +5,34 @@ int main() {
     vector<int> v;
     cout<<"Capacity:"<<v.capacity()<<endl;
 
-    v.push_back(1);
+    v.push_back(4);
     cout<<"Capacity:"<<v.capacity()<<endl;
 
-    v.push_back(2);
+    v.push_back(7);
     cout<<"Capacity:"<<v.capacity()<<endl;
 
     v.push_back(3);
+    cout<<"Capacity:"<<v.capacity()<<endl;
+    
+    v.push_back(1);
     cout<<"Capacity:"<<v.capacity()<<endl;
 
     cout<<"Element at 2nd Index"<<v.at(2)<<endl;
 
     cout<<"Front:"<<v.front()<<endl;
     cout<<"Back:"<<v.back()<<endl;
+    
+    cout<<"Before sorting :";
+    for(int i: v){
+        cout<<i<<" ";
+    } cout<<endl;
+    sort(v.begin(), v.end());
+    cout<<"After sorting :";
+    for(int i: v){
+        cout<<i<<" ";
+    } cout<<endl;
+    
+    
 
     cout<<"Before POP:"<<endl;
     for(int i:v) {
@@ -28,7 +43,8 @@ int main() {
     cout<<"After pop"<<endl;
     for(int i:v) {
         cout<<i<<" ";
-    }
+    } cout<<endl;
+    
     cout<<"Before Clear Size:"<<v.size()<<endl;
     v.clear();
     cout<<"After clear size:"<<v.size()<<endl;
